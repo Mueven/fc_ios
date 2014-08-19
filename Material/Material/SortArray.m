0aadef764b219ccab2623ef9bf46a8d059dc9d1d
wayne <zixiao.wang@cz-tek.com>
2014-08-19 16:49:44 +0800

    review tuo

--- Material/Material/SortArray.m
+++ Material/Material/SortArray.m
@@ -13,27 +13,29 @@
 +(NSArray *)sortByPartNumber:(NSArray *)originArray
 {
     NSMutableArray *origin=[originArray mutableCopy];
-    NSMutableArray *newArray=[NSMutableArray array];
+    NSMutableArray *newArray=[[NSMutableArray alloc] init];
     NSString *partNumber=[NSString string];
-    NSMutableDictionary *currentItem=[NSMutableDictionary dictionary];
-    [currentItem setObject:[NSMutableArray array] forKey:@"xiangArray"];
+    NSMutableDictionary *currentItem=[[NSMutableDictionary alloc] init];
+    NSMutableArray *tempArray=[[NSMutableArray alloc] init];
+    [currentItem setObject:tempArray forKey:@"xiangArray"];
+    
     while(origin.count>0){
         int length=origin.count;
         for(int i=length-1;i>=0;i--){
+            Xiang *xiang=origin[i];
             if(i==length-1){
-                partNumber=[origin[i] objectForKey:@"number"];
+                partNumber=xiang.number;
                 [currentItem setObject:partNumber forKey:@"partNumber"];
-                [currentItem[@"xiangArray"] addObject:[origin[i] copy]];
+                [currentItem[@"xiangArray"] addObject:[[Xiang alloc] copyMe:xiang]];
                 [origin removeObjectAtIndex:i];
                 if(i==0){
                     [newArray addObject:[currentItem copy]];
-                    break ;
                 }
             }
             else{
-                NSString *currentPartNumber=[origin[i] objectForKey:@"number"];
+                NSString *currentPartNumber=xiang.number;
                 if([currentPartNumber isEqualToString:partNumber]){
-                    [currentItem[@"xiangArray"] addObject:[origin[i] copy]];
+                    [currentItem[@"xiangArray"] addObject:[[Xiang alloc] copyMe:xiang]];
                     [origin removeObjectAtIndex:i];
                 }
                 if(i==0){
@@ -43,6 +45,7 @@
             }
         }
     }
+ 
     return [newArray copy];
 }
 @end
