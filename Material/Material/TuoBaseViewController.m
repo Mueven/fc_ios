7cd87a54de7ba36c3af460a0b02c6cb8b8bd2935
wayne <wayneup@163.com>
2014-11-11 11:06:14 +0800

    test

--- Material/Material/TuoBaseViewController.m
+++ Material/Material/TuoBaseViewController.m
@@ -32,7 +32,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    // Do any additional setup after loading the view.//test comment
     self.department.delegate=self;
     self.agent.delegate=self;
     
